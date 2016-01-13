243423818af09e8fa5f0c8d890dd1337c273d8f2
Armin Kuster <akuster808@gmail.com>
2016-01-13 08:16:29 -0800

    Constants update to use sqlalchemy
    Signed-off-by: Armin Kuster <akuster808@gmail.com>

--- Nvd/Constants.py
+++ Nvd/Constants.py
@@ -8,6 +8,7 @@
 # ex:ts=4:sw=4:sts=4:et
 # -*- tab-width: 4; c-basic-offset: 4; indent-tabs-mode: nil -*-
 import os
+import sys
 import platform
 
 __version__ = '7.9'
@@ -33,93 +34,80 @@ plat = platform.system()
 if plat == 'Darwin':
     working_dir="/Users/Shared/%s" % __name__
 elif plat == 'Linux' :
-    working_dir="/var/cve/db/%s" % __name__
+    #working_dir="/var/cve/db/%s" % __name__
+    working_dir=os.path.join(os.path.abspath(os.path.dirname(os.path.abspath(sys.argv[0]))),"db","%s"%  __name__)
 else:
     print "Un-supported os"
     sys.exit(2)
 
-DBname = 'nvdcve.db'
 data = working_dir+"/data"
 cveweb = '/Users/Shared/cve'
 cvetmpdata= '/tmp/cve/data'
 
 nvdcve_modified_url = ["https://nvd.nist.gov/download/nvdcve-modified.xml"]
 nvdcve_recent_url = ["https://nvd.nist.gov/download/nvdcve-recent.xml"]
-nvdcve_urls = [
-    "https://nvd.nist.gov/download/nvdcve-2002.xml",
-    "https://nvd.nist.gov/download/nvdcve-2003.xml",
-    "https://nvd.nist.gov/download/nvdcve-2004.xml",
-    "https://nvd.nist.gov/download/nvdcve-2005.xml",
-    "https://nvd.nist.gov/download/nvdcve-2006.xml",
-    "https://nvd.nist.gov/download/nvdcve-2007.xml",
-    "https://nvd.nist.gov/download/nvdcve-2008.xml",
-    "https://nvd.nist.gov/download/nvdcve-2009.xml",
-    "https://nvd.nist.gov/download/nvdcve-2010.xml",
-    "https://nvd.nist.gov/download/nvdcve-2011.xml",
-    "https://nvd.nist.gov/download/nvdcve-2012.xml",
-]
 
-# array - [db name, db datatype]
+# array - [Title, table name, db datatype, displable]
 
 ver_db = [
-    ['Cve id', 'cve_id', 'INTEGER', False],
-    ['Vendor', 'vendor', 'TEXT', True],
-    ['Product', 'prod', 'TEXT', True],
-    ['Edition', 'edition', 'TEXT', True],
-    ['Num', 'num', 'TEXT', True]
+    ['Cve id', 'cve_id', '', False],
+    ['Vendor', 'vendor', 'String', True],
+    ['Product', 'prod', 'String', True],
+    ['Edition', 'edition', 'String', True],
+    ['Num', 'num', 'String', True]
     ]
 
 desc_db = [
-    ['Source', 'source', 'TEXT', True],
-    ['Desc', 'desc', 'BLOB', True]
+    ['Source', 'source', 'String', True],
+    ['Desc', 'desc', 'String', True]
     ]
 
 sol_db = [
-    ['Source', 'source', 'TEXT', True],
-    ['Data', 'data', 'BLOB', True]
+    ['Source', 'source', 'String', True],
+    ['Data', 'data', 'String', True]
     ]
 
 cve_db = [
-    ['Seq', 'seq', 'VARCHAR(10)', True],
-    ['CVSS_vector', 'CVSS_vector', 'TEXT', True],
-    ['CVSS_base_score', 'CVSS_base_score', 'VARCHAR(5)', True],
-    ['CVSS_exploit_subscore', 'CVSS_exploit_subscore', 'VARCHAR(5)', True],
-    ['CVSS_impact_subscore', 'CVSS_impact_subscore', 'VARCHAR(5)', True],
-    ['name', 'name', 'VARCHAR(14)', True],
-    ['severity', 'severity', 'VARCHAR(7)', True],
-    ['type', 'type', 'VARCHAR(4)', True],
-    ['published', 'published', 'DATE', True],
-    ['CVSS_version', 'CVSS_version', 'VARCHAR(5)', True],
-    ['CVSS_score', 'CVSS_score', 'VARCHAR(5)', True],
-    ['modified', 'modified', 'DATE', True],
-    ['desc_id', 'desc_id', 'INTEGER', False],
-    ['sol_id', 'sol_id', 'INTEGER', False],
-    ['Vendor','vendor', 'TEXT', True],
-    ['Product','prod', 'TEXT', True],
+    ['Seq', 'seq', 'String(10)', True],
+    ['CVSS_vector', 'CVSS_vector', 'String', True],
+    ['CVSS_base_score', 'CVSS_base_score', 'String(5)', True],
+    ['CVSS_exploit_subscore', 'CVSS_exploit_subscore', 'String(5)', True],
+    ['CVSS_impact_subscore', 'CVSS_impact_subscore', 'String(5)', True],
+    ['name', 'name', 'String(14)', True],
+    ['severity', 'severity', 'String(7)', True],
+    ['type', 'type', 'String(4)', True],
+    ['published', 'published', 'Date', True],
+    ['CVSS_version', 'CVSS_version', 'String(5)', True],
+    ['CVSS_score', 'CVSS_score', 'String(5)', True],
+    ['modified', 'modified', 'Date', True],
+    ['desc_id', 'desc_id', 'Integer', False],
+    ['sol_id', 'sol_id', 'Integer', False],
+    ['Vendor','vendor', 'String', True],
+    ['Product','prod', 'String', True],
     ]
 
 ref_db = [
-    ['Cve id', 'cve_id', 'INTEGER', False],
-    ['Source', 'source', 'TEXT', True],
-    ['Patch', 'patch', 'VARCHAR(2)', True],
-    ['URL', 'url', 'BLOB', True],
-    ['Adv', 'adv', 'VARCHAR(2)', True],
-    ['Data', 'data', 'TEXT', True],
+    ['Cve id', 'cve_id', 'Integer', False],
+    ['Source', 'source', 'String', True],
+    ['Patch', 'patch', 'String(2)', True],
+    ['URL', 'url', 'String', True],
+    ['Adv', 'adv', 'String(2)', True],
+    ['Data', 'data', 'String', True],
     ]
 
 range_db = [
-    ['Cve id', 'cve_id', 'INTEGER', False],
-    ['Data', 'data', 'VARCHAR(10)', True],
+    ['Cve id', 'cve_id', 'Integer', False],
+    ['Data', 'data', 'String(10)', True],
     ]
 
 loss_types_db = [
-    ['Cve id', 'cve_id', 'INTEGER', False],
-    ['Type', 'type', 'VARCHAR(6)', True],
+    ['Cve id', 'cve_id', 'Integer', False],
+    ['Type', 'type', 'String(6)', True],
     ]
 
 sec_prot_db = [
-    ['Loss type id', 'loss_type_id', 'INTEGER', False],
-    ['type', 'type', 'VARCHAR(6)', True],
-    ['data', 'data', 'TEXT', True]
+    ['Loss type id', 'loss_type_id', 'Integer', False],
+    ['type', 'type', 'String(6)', True],
+    ['data', 'data', 'String', True]
     ]
 
